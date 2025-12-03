0d80be852aeb5e410ffc39e47220170a78b42d29
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-03 11:36:17 +0100

    I've added configuration for OAuth passport for both Google and Discord

---
+++ src/config/passport.ts
@@ -0,0 +1,73 @@
+import dotenv from "dotenv";
+import passport, { Profile } from "passport"
+import { AppDataSource } from "../data-source";
+import { User } from "../entities/User";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20"
+import { Strategy as DiscordStrategy } from "passport-discord"
+
+dotenv.config({ quiet: true });
+
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL,
+    DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, DISCORD_CALLBACK_URL
+} = process.env
+
+passport.serializeUser((user: any, done) => done(null, user));
+passport.deserializeUser((obj: any, done) => done(null, obj));
+
+const createOrFetchuser = async (profile: Profile, provider: string) => {
+    const userRepository = AppDataSource.getRepository(User);
+    const email = profile.emails?.[0].value
+
+    if (!email) {
+        throw new Error("No email found in Google profile")
+    }
+
+    let user = await userRepository.findOne({ where: { email } })
+
+    if (!user) {
+        user = userRepository.create({
+            email,
+            name: profile.displayName,
+            provider,
+            providerId: profile.id
+        })
+        await userRepository.save(user)
+    }
+    return user
+}
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: GOOGLE_CLIENT_ID!,
+            clientSecret: GOOGLE_CLIENT_SECRET!,
+            callbackURL: GOOGLE_CALLBACK_URL
+        },
+        async (accessToken: string, refreshToken: string, profile: Profile, done: any) => {
+            try {
+                const user = await createOrFetchuser(profile, "google")
+                return done(null, user)
+            } catch (error) {
+                return done(error, null)
+            }
+        }
+    )
+)
+
+passport.use(
+    new DiscordStrategy(
+        {
+            clientID: DISCORD_CLIENT_ID!,
+            clientSecret: DISCORD_CLIENT_SECRET!,
+            callbackURL: DISCORD_CALLBACK_URL
+        },
+        async (accessToken: string, refreshToken: string, profile: Profile, done: any) => {
+            try {
+                const user = await createOrFetchuser(profile, "discord")
+                return done(null, user)
+            } catch (error) {
+                return done(error, null)
+            }
+        }
+    )
+)
