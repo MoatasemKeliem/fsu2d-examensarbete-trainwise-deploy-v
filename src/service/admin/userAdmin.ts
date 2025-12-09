e1a32fdf6e6f3bcc23c42a14beac89781a2c0b3c
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-09 17:59:16 +0100

    Removed so that the hashed password isn't sent to frontend

--- src/service/admin/userAdmin.ts
+++ src/service/admin/userAdmin.ts
@@ -11,7 +11,14 @@ export const getAllUsers = async (req: Request, res: Response) => {
     try {
         const users = await userRepository.find();
 
-        return res.json({ status: 200, users })
+        const allUsers = users.map((user) => {
+            return {
+                ...user,
+                password: ""
+            }
+        })
+
+        return res.json({ status: 200, users: allUsers })
     } catch (error) {
         console.error("Couldn't get all users")
         return res.json({ status: 500, message: "Users aren't avalible right now" })
@@ -28,11 +35,13 @@ export const getUsersById = async (req: Request, res: Response) => {
             return res.json({ status: 400, message: "User ID doesn't exist" })
         }
 
-        const user = await userRepository.findOne({ where: { id } })
+        const userById = await userRepository.findOne({ where: { id } })
 
-        if (!user) {
+        if (!userById) {
             return res.json({ status: 404, message: "User not found" })
         }
+
+        const user = { ...userById, password: "" }
 
         return res.json({ status: 200, user })
 
@@ -59,9 +68,11 @@ export const deleteUsersById = async (req: Request, res: Response) => {
             return res.json({ status: 404, message: "User not found" })
         }
 
+        const userToDelete = { ...user, password: "" }
+
         await userRepository.delete(id)
 
-        return res.json({ status: 200, message: "" })
+        return res.json({ status: 200, message: `User with the ID of: ${id} was deleted successfully`, user: userToDelete })
 
     } catch (error) {
         console.error("Couldn't get all users")
