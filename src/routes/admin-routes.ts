5fee9b59286ede0a139ff0dc9aca17398b0eb751
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-09 14:47:33 +0100

    Added Training log CMS for admin

--- src/routes/admin-routes.ts
+++ src/routes/admin-routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authMiddleware } from "../middleware/auth-middleware";
 import { AdminDeletetrainingPlanById, AdminGetAllTrainingPlans, AdminGetTrainingPlanById } from "../service/admin/trainingPlan-Admin-service";
 import { AdminDeletetNutritionPlanById, AdminGetAllNutritionPlans, AdminGetNutritionPlanById } from "../service/admin/nutritionPlan-Admin-service";
+import { AdminDeletetrainingLogById, AdminGetAllTrainingLogs, AdminGetTrainingLogById } from "../service/admin/trainingLog-Admin-service";
 
 
 
@@ -14,5 +15,9 @@ adminRoutes.delete("/training-plan/:id", authMiddleware, AdminDeletetrainingPlan
 adminRoutes.get("/nutrition-plan/", authMiddleware, AdminGetAllNutritionPlans)
 adminRoutes.get("/nutrition-plan/:id", authMiddleware, AdminGetNutritionPlanById)
 adminRoutes.delete("/nutrition-plan/:id", authMiddleware, AdminDeletetNutritionPlanById)
+
+adminRoutes.get("/training-log/", authMiddleware, AdminGetAllTrainingLogs)
+adminRoutes.get("/training-log/:id", authMiddleware, AdminGetTrainingLogById)
+adminRoutes.delete("/training-log/:id", authMiddleware, AdminDeletetrainingLogById)
 
 export default adminRoutes;
