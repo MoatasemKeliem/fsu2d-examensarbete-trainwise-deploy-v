bda6078668268c49b9b14d63fe38c0d378abe410
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-13 16:25:17 +0100

    Created the render page for rendering single training plan

--- client/src/pages/SingleTrainingPlan.tsx
+++ client/src/pages/SingleTrainingPlan.tsx
@@ -1,8 +1,70 @@
+import { useEffect } from "react"
+import useTrainingPlan from "../hooks/useTrainingPlan"
+import { useParams } from "react-router-dom"
 
 const SingleTrainingPlan = () => {
+    const { getTrainingPlanById, trainingPlanById } = useTrainingPlan()
+    const { id } = useParams()
+
+    useEffect(() => {
+        if (!id) {
+            return
+        }
+        getTrainingPlanById(id)
+    }, [])
+
+    console.log(trainingPlanById)
+    if (!trainingPlanById) {
+        return (
+            <div>
+                <h2>Training plan with the ID of: {id} doesn't exist</h2>
+            </div>
+        )
+    }
     return (
         <div>
             <h1>Single Training Plan</h1>
+            {trainingPlanById?.trainingPlan.title}
+            <p>Created at: {trainingPlanById.trainingPlan.createdAt.slice(0, 10)}</p>
+
+            <div>
+                {
+                    trainingPlanById.trainingPlan.plan.map((plan) => {
+                        return (
+                            <div key={plan.week}>
+                                <h3>{plan.week}</h3>
+                                {
+                                    plan.days.map((day) => {
+                                        return (
+                                            <div key={day.day}>
+                                                <h4>{day.day}</h4>
+                                                <h5>Exercises</h5>
+                                                <div>
+                                                    {
+                                                        day.exercises.map((exercies, index: number) => {
+                                                            return (
+                                                                <div key={index}>
+                                                                    <span>{exercies.name} - {exercies.sets} sets - {exercies.reps} reps</span>
+                                                                </div>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                                <p>Calories burned: {day.caloriesBurned} kcal</p>
+                                                <p>Motivation: {day.motivation}</p>
+                                                <p>Nutrition tips: {day.nutritionTips}</p>
+                                            </div>
+                                        )
+                                    })
+                                }
+
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+            <button>Delete training plan</button>
         </div>
     )
 }
