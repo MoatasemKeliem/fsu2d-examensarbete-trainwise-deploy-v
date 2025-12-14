df696159636d30d45d2d3beabd63ec3c7cfb926f
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-14 12:17:52 +0100

    added rendering for all nutrition plans

---
+++ client/src/components/NutritionPlansComponent.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react"
+import useNutrition from "../hooks/useNutrition"
+import { Link } from "react-router-dom"
+
+const NutritionPlansComponent = () => {
+    const { getAllNutritionPlans, allNutrition } = useNutrition()
+
+    useEffect(() => {
+        getAllNutritionPlans()
+    }, [])
+
+    console.log(allNutrition)
+
+    return (
+        <div>
+            {
+                allNutrition.map((plan) => {
+                    return (
+                        <div key={plan.id}>
+                            <h2>{plan.title}</h2>
+                            <p>{plan.createdAt.slice(0, 10)}</p>
+                            <Link to={`/nutritions/${plan.id}`}><button>View Nutrition Plan</button></Link>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default NutritionPlansComponent
