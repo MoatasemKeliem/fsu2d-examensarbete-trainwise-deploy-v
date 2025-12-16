4f0a6bc0ff2568e229924ce8da1093e13618660e
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-16 14:47:38 +0100

    Fixed backend response status and added fetch hook function in frontend

---
+++ client/src/hooks/Admin/useNutritionPlanAdmin.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import type { INutritionPlans } from '../../model/render-models/InutritionPlans'
+import axios from 'axios'
+import { Backend_URL } from '../../utils'
+
+const useNutritionPlanAdmin = () => {
+    const [allNutritionPlansAdmin, setAllNutritionPlansAdmin] = useState<INutritionPlans[]>([])
+
+    const getAllNutritionPlanAdmin = async () => {
+        try {
+            const resposne = await axios.get(`${Backend_URL}/admin/nutrition-plan`, { withCredentials: true })
+            const data = resposne.data
+            setAllNutritionPlansAdmin(data.nutritionPlan)
+        } catch (error) {
+            console.error("Couldn't get nutrition plan for admin: ", error)
+            throw new Error()
+        }
+    }
+
+    return { getAllNutritionPlanAdmin, allNutritionPlansAdmin }
+}
+
+export default useNutritionPlanAdmin
