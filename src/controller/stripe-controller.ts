7a369ed6cadf265f0084c4a0c2a110d7c57c0de2
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-18 12:04:43 +0100

    Created a new endpoint for the payment and restructured the stripe payment to not include webbhooks might bring it back later

--- src/controller/stripe-controller.ts
+++ src/controller/stripe-controller.ts
@@ -6,26 +6,21 @@ import { Subscription } from "../entities/Subscription"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
-export const stripePayment = async (req: Request, res: Response) => {
+export const createPayment = async (req: Request, res: Response) => {
     const userId = (req as any).userId
     const { priceId } = req.body
-
-    if (!userId) {
-        return res.json({ status: 400, message: "User not found" })
-    }
 
     try {
         const userRepository = AppDataSource.getRepository(User)
         const subscriptionRepository = AppDataSource.getRepository(Subscription)
-
-
         const user = await userRepository.findOne({ where: { id: userId } })
 
         if (!user) {
-            return res.json({ status: 400, message: "User not found" })
+            return res.status(400).json({ message: "User not found" })
         }
 
         let stripeCustomerId = user.stripeCustomerId;
+
 
         if (!stripeCustomerId) {
             const customer = await stripe.customers.create({
@@ -38,26 +33,66 @@ export const stripePayment = async (req: Request, res: Response) => {
             await userRepository.save(user)
         }
 
-        const session = await stripe.subscriptions.create({
+        const setupIntent = await stripe.setupIntents.create({
             customer: stripeCustomerId,
-            items: [{ price: priceId, quantity: 1 }],
-            payment_behavior: "default_incomplete",
+            automatic_payment_methods: { enabled: true }
+        })
+
+        return res.json({ setupIntent, priceId })
+
+
+    } catch (error) {
+        console.error(error)
+        return res.json({ status: 500, message: "Couldn't create payment" })
+    }
+}
+
+
+export const stripePayment = async (req: Request, res: Response) => {
+    const userId = (req as any).userId
+    const { priceId, paymentMethodId } = req.body
+
+    if (!userId) {
+        return res.json({ status: 400, message: "User not found" })
+    }
+
+    try {
+        const userRepository = AppDataSource.getRepository(User)
+        const subscriptionRepository = AppDataSource.getRepository(Subscription)
+
+
+        const user = await userRepository.findOne({ where: { id: userId } })
+
+        if (!user || !user.stripeCustomerId) {
+            return res.status(400).json({ message: "User not found" })
+        }
+
+
+        const session = await stripe.subscriptions.create({
+            customer: user.stripeCustomerId,
+            items: [{ price: priceId, }],
+            default_payment_method: paymentMethodId,
             expand: ["latest_invoice.payment_intent"],
             metadata: {
                 userId: user.id
             }
 
 
+
+
+
         })
         const invoice = session.latest_invoice as Stripe.Invoice & { payment_intent?: Stripe.PaymentIntent }
         const clientSecret = invoice.payment_intent?.client_secret
 
+
+
         const subscriptionTable = subscriptionRepository.create({
             stripeSubscription: session.id,
-            stripeCustomerId,
+            stripeCustomerId: user.stripeCustomerId,
             user,
             planId: priceId,
-            status: "inactive"
+            status: "active"
         })
 
         await subscriptionRepository.save(subscriptionTable);
@@ -70,55 +105,58 @@ export const stripePayment = async (req: Request, res: Response) => {
     }
 }
 
-export const stripeWebhook = async (req: Request, res: Response) => {
-    const signal = req.headers["stripe-signature"] as string;
-    const webhooksSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
-    try {
-        const event = stripe.webhooks.constructEvent(req.body, signal, webhooksSecret)
 
-        const subscriptionRepository = AppDataSource.getRepository(Subscription)
 
-        if (event.type === "invoice.paid") {
-            const invoice = event.data.object as Stripe.Invoice & { subscription?: string }
-            const subscriptionId = invoice.subscription
+// export const stripeWebhook = async (req: Request, res: Response) => {
+//     const signal = req.headers["stripe-signature"] as string;
+//     const webhooksSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
-            if (subscriptionId) {
-                const subscription = await subscriptionRepository.findOne({ where: { stripeSubscription: subscriptionId } })
+//     try {
+//         const event = stripe.webhooks.constructEvent(req.body, signal, webhooksSecret)
 
-                if (subscription) {
-                    subscription.status = "active";
-                    await subscriptionRepository.save(subscription)
-                    console.log("Subscription atcivated: ", event.data.object)
+//         const subscriptionRepository = AppDataSource.getRepository(Subscription)
 
-                }
-            }
+//         if (event.type === "invoice.paid") {
+//             const invoice = event.data.object as Stripe.Invoice & { subscription?: string }
+//             const subscriptionId = invoice.subscription
 
-        } else if (event.type === "customer.subscription.updated") {
-            const deletedSubscription = event.data.object as Stripe.Subscription
-            const subscriptionId = deletedSubscription.id
+//             if (subscriptionId) {
+//                 const subscription = await subscriptionRepository.findOne({ where: { stripeSubscription: subscriptionId } })
 
-            if (subscriptionId) {
-                const subscription = await subscriptionRepository.findOne({ where: { stripeSubscription: subscriptionId } })
+//                 if (subscription) {
+//                     subscription.status = "active";
+//                     await subscriptionRepository.save(subscription)
+//                     console.log("Subscription atcivated: ", event.data.object)
 
-                if (subscription) {
-                    subscription.status = "inactive";
-                    await subscriptionRepository.save(subscription)
-                    console.log("Subscription deleted: ", event.data.object)
-                }
-            }
-        } else {
-            console.log("Incomplete event: ", event.type)
-        }
+//                 }
+//             }
 
-        return res.json({ status: 200, message: "Event accomplished" })
+//         } else if (event.type === "customer.subscription.updated") {
+//             const deletedSubscription = event.data.object as Stripe.Subscription
+//             const subscriptionId = deletedSubscription.id
 
-    } catch (error) {
-        console.error("Webhook failed", error)
-        return res.json({ status: 500, message: "Webook failed to verify" })
-    }
+//             if (subscriptionId) {
+//                 const subscription = await subscriptionRepository.findOne({ where: { stripeSubscription: subscriptionId } })
 
-}
+//                 if (subscription) {
+//                     subscription.status = "inactive";
+//                     await subscriptionRepository.save(subscription)
+//                     console.log("Subscription deleted: ", event.data.object)
+//                 }
+//             }
+//         } else {
+//             console.log("Incomplete event: ", event.type)
+//         }
+
+//         return res.json({ status: 200, message: "Event accomplished" })
+
+//     } catch (error) {
+//         console.error("Webhook failed", error)
+//         return res.json({ status: 500, message: "Webook failed to verify" })
+//     }
+
+// }
 
 export const cancelSubscription = async (req: Request, res: Response) => {
     const { subscriptionId } = req.body
--- src/routes/stripe-routes.ts
+++ src/routes/stripe-routes.ts
@@ -1,11 +1,13 @@
 import express, { Router } from "express"
-import { cancelSubscription, stripePayment } from "../controller/stripe-controller"
+import { cancelSubscription, createPayment, stripePayment } from "../controller/stripe-controller"
+import { authMiddleware } from "../middleware/auth-middleware"
 
 const stripeRoute = Router()
 
 
-stripeRoute.post("/create-payment", stripePayment)
-stripeRoute.post("/cancel-payment", cancelSubscription)
+stripeRoute.post("/create-payment", authMiddleware, stripePayment)
+stripeRoute.post("/create-subscription", authMiddleware, createPayment)
+stripeRoute.post("/cancel-payment", authMiddleware, cancelSubscription)
 
 
 export default stripeRoute;
