fcccc7bdf5269ad5a339fc5d25f460ce58522430
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-19 14:57:19 +0100

    added the subscription middleware to the necessary endpoints

--- src/routes/article-routes.ts
+++ src/routes/article-routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { deleteArticleById, getAllArticles, getArticleById } from "../service/CRUD/article-CRUD-service";
 import { authMiddleware } from "../middleware/auth-middleware";
+import { subscriptionMiddleware } from "../middleware/subscriptionMiddleware";
 
 
 
 const articleRoute = Router()
 
 
-articleRoute.get("/", authMiddleware, getAllArticles)
-articleRoute.get("/:id", authMiddleware, getArticleById)
-articleRoute.delete("/:id", authMiddleware, deleteArticleById)
+articleRoute.get("/", authMiddleware, subscriptionMiddleware, getAllArticles)
+articleRoute.get("/:id", authMiddleware, subscriptionMiddleware, getArticleById)
+articleRoute.delete("/:id", authMiddleware, subscriptionMiddleware, deleteArticleById)
 
 
 export default articleRoute;
--- src/routes/nutritionPlan-routes.ts
+++ src/routes/nutritionPlan-routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth-middleware";
 import { deletetNutritionPlanById, getAllNutritionPlans, getNutritionPlanById } from "../service/CRUD/nutritionPlan-CRUD-service";
+import { subscriptionMiddleware } from "../middleware/subscriptionMiddleware";
 
 
 
 const nutritionPlanRoutes = Router()
 
-nutritionPlanRoutes.get("/", authMiddleware, getAllNutritionPlans)
-nutritionPlanRoutes.get("/:id", authMiddleware, getNutritionPlanById)
-nutritionPlanRoutes.delete("/:id", authMiddleware, deletetNutritionPlanById)
+nutritionPlanRoutes.get("/", authMiddleware, subscriptionMiddleware, getAllNutritionPlans)
+nutritionPlanRoutes.get("/:id", authMiddleware, subscriptionMiddleware, getNutritionPlanById)
+nutritionPlanRoutes.delete("/:id", authMiddleware, subscriptionMiddleware, deletetNutritionPlanById)
 
 
 
