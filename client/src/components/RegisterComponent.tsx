6e11e2704c3b51dad46e8e2cffd168dad5e6bd37
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-21 12:47:35 +0100

    Added CSS to register form and login form

--- client/src/components/RegisterComponent.tsx
+++ client/src/components/RegisterComponent.tsx
@@ -2,6 +2,8 @@ import { useState, type FormEvent } from "react"
 import useAccount from "../hooks/useAccount"
 import type { IRegister } from "../model/IRegister"
 import { Link } from "react-router-dom"
+import "../style/account.css"
+
 
 const RegisterComponent = () => {
     const { registerUser } = useAccount()
@@ -18,21 +20,23 @@ const RegisterComponent = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">Name <br />
-                    <input type="text" name="name" value={register.name} onChange={(e) => setRegister({ ...register, name: e.target.value })} required /> <br />
-                </label><br />
-                <label htmlFor="">Email <br />
-                    <input type="email" name="email" value={register.email} onChange={(e) => setRegister({ ...register, email: e.target.value })} required /> <br />
-                </label><br />
-                <label htmlFor="">Password <br />
-                    <input type="password" name="password" value={register.password} onChange={(e) => setRegister({ ...register, password: e.target.value })} required /> <br />
-                </label><br />
-                <button type="submit">Register</button>
-            </form>
-            <Link to={"/login"}>Already have an account</Link>
-        </div>
+        <section className="account-page">
+            <div className="account-div">
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="">Name <br />
+                        <input type="text" name="name" value={register.name} onChange={(e) => setRegister({ ...register, name: e.target.value })} required /> <br />
+                    </label><br />
+                    <label htmlFor="">Email <br />
+                        <input type="email" name="email" value={register.email} onChange={(e) => setRegister({ ...register, email: e.target.value })} required /> <br />
+                    </label><br />
+                    <label htmlFor="">Password <br />
+                        <input type="password" name="password" value={register.password} onChange={(e) => setRegister({ ...register, password: e.target.value })} required /> <br />
+                    </label><br />
+                    <button type="submit">Register</button>
+                </form>
+                <Link to={"/login"}><button className="button">Already have an account</button></Link>
+            </div>
+        </section>
     )
 }
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,7 +3,6 @@ import LoginComponent from "../components/LoginComponent"
 const Login = () => {
     return (
         <div>
-            <h1>Login</h1>
             <LoginComponent />
         </div>
     )
