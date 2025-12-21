20a21cffa267d80054c4bdab16bcbbfee1f02f67
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-21 13:56:25 +0100

    Added JSX and CSS to contact page

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom"
+import "../style/account.css"
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <section className="account-page">
+            <div>
+                <form className='account-div'>
+                    <h3>Contact Us</h3>
+                    <label> Name <br />
+                        <input type="text" placeholder="Name..." />
+                    </label><br />
+
+                    <label> Email <br />
+                        <input type="email" placeholder="Email..." />
+                    </label><br />
+
+
+                    <label> Message<br />
+                        <textarea name="" id="" placeholder="Your message..."></textarea>
+                    </label><br />
+                    <Link to={"/"}><button>Send Message</button></Link>
+
+                </form>
+            </div>
+        </section>
+
     )
 }
 
