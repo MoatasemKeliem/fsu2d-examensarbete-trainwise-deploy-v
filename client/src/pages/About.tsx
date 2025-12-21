50f4d5431ac4fda1a26142bd7ceab674e91ff483
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-21 14:12:35 +0100

    added css style to abput us

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,8 +1,29 @@
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div id="about-page">
+            <section id="about-container">
+                <h1>About TrainWise</h1>
+
+                <h2>Our mission is to make fitness smarter, more personal and accessible for everyone</h2>
+
+                <p>TrainWise is a platform that combines fitness expertise with AI to help users reach their goals effectively.
+                    By understanding each users goal, experience level and much more. TrainWise creates personlized training and nutrition plans
+                    that suits each user needs.
+                </p>
+
+                <h2>Features our users can user:</h2>
+                <ul>
+                    <li>Generate AI training and nutrition plans</li>
+                    <li>Log training and nutrition progression </li>
+                    <li>Get AI feedback of progression</li>
+                    <li>Learn from our articles</li>
+                </ul>
+
+                <p>We belive fitness should be accessible by anyone. We belive fitness should be personlized and motiviting. Our goal is to help
+                    users train smarter, stay consistent and achieve theri goals.
+                </p>
+            </section>
         </div>
     )
 }
