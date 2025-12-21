4e0a894322123cd6c437b028dfc333c07c9154fe
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-21 12:21:11 +0100

    Ladning page is done with css

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbars/Navbar'
+import Footer from './Footer'
 
 const Layout = () => {
     return (
         <div>
             <header><Navbar /></header>
             <main><Outlet /></main>
-            <footer>FOOTER</footer>
+            <footer><Footer /></footer>
         </div>
     )
 }
