b4936f19222946f980e2adb0f328da9585b3845d
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-22 20:59:24 +0100

    added style to training log

--- client/src/components/TrainingLogComponent.tsx
+++ client/src/components/TrainingLogComponent.tsx
@@ -16,17 +16,14 @@ const TrainingLogComponent = () => {
 
 
     return (
-        <div>
+        <div className='render-page-div'>
             {
                 allTrainingLogs.map((log) => {
                     return (
-                        <div key={log.id}>
-                            <h2>Your workout summary</h2>
-                            <p>{log.workoutSummary}</p>
+                        <div className='render-item' key={log.id}>
+                            <p className='render-workoutSummary'>{log.workoutSummary.slice(0, 200)}...</p>
 
-                            <h2>AI feedback</h2>
-                            <p>{log.aiFeedback}</p>
-                            <p>Created at: {log.createdAt.slice(0, 10)}</p>
+                            <p className='render-date'>{log.createdAt.slice(0, 10)}</p>
 
                             <Link to={`/training-logs/${log.id}`}><button>View Training Log</button></Link>
 
--- client/src/pages/TrainingLogs.tsx
+++ client/src/pages/TrainingLogs.tsx
@@ -3,7 +3,6 @@ import TrainingLogComponent from "../components/TrainingLogComponent"
 const TrainingLogs = () => {
     return (
         <div>
-            <h1>Training Logs</h1>
             <TrainingLogComponent />
         </div>
     )
