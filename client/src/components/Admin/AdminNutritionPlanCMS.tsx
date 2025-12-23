af5f4e2e4e0b60fd741c7718bc6688db0d2c7f20
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-23 12:47:43 +0100

    Added CSS to admin all nutrition plans

--- client/src/components/Admin/AdminNutritionPlanCMS.tsx
+++ client/src/components/Admin/AdminNutritionPlanCMS.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import useNutritionPlanAdmin from '../../hooks/Admin/useNutritionPlanAdmin'
 import { Link } from 'react-router-dom'
+import "../../style/render.css"
+
 
 const AdminNutritionPlanCMS = () => {
     const { getAllNutritionPlanAdmin, allNutritionPlansAdmin } = useNutritionPlanAdmin()
@@ -11,17 +13,17 @@ const AdminNutritionPlanCMS = () => {
 
 
     return (
-        <div>
+        <div className='render-page-div'>
             {
                 allNutritionPlansAdmin.map((plan) => {
                     return (
-                        <div key={plan.id}>
-                            <h2>Title: {plan.title}</h2>
-                            <p>Created At: {plan.createdAt.slice(0, 10)}</p>
+                        <div className='render-item-admin' key={plan.id}>
+                            <h2 className='render-sub-title'>{plan.title}</h2>
+                            <p className='render-date'>{plan.createdAt.slice(0, 10)}</p>
                             <div>
-                                <p>Name: {plan.user.name}</p>
-                                <p>Email: {plan.user.email}</p>
-                                <p>Role: {plan.user.role}</p>
+                                <p><span className='render-user-info'>Name:</span> {plan.user.name}</p>
+                                <p><span className='render-user-info'>Email:</span> {plan.user.email}</p>
+                                <p><span className='render-user-info'>Role:</span> {plan.user.role}</p>
                             </div>
                             <Link to={`/admin-nutrition-Plan/${plan.id}`}><button>View Nutrition Plan</button></Link>
 
