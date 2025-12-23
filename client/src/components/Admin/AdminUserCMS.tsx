74fb6f20b0f0e704e650c4d581f6795d92cca7d3
moatasemKeliem <keliemmoatasem@gmail.com>
2025-12-23 15:30:00 +0100

    added CSS to admin user and done with CSS styling for single training plan

--- client/src/components/Admin/AdminUserCMS.tsx
+++ client/src/components/Admin/AdminUserCMS.tsx
@@ -14,17 +14,18 @@ const AdminUserCMS = () => {
 
 
     return (
-        <div>
+        <div className='render-user-div'>
+
             {
                 allUsers.map((user) => {
                     return (
-                        <div key={user.id}>
-                            <h2>Name: {user.name}</h2>
-                            <p>Email: {user.email}</p>
-                            <p>Role: {user.role}</p>
-                            <p>Provider: {user.provider}</p>
-                            <p>Stripe customer ID: {user.stripeCustomerId}</p>
-                            <p>Created: {user.createdAt.slice(0, 10)}</p>
+                        <div className='render-user-admin' key={user.id}>
+                            <h2><span className='render-user-info'>Name:</span> {user.name}</h2>
+                            <p><span className='render-user-info'>Email:</span> {user.email}</p>
+                            <p><span className='render-user-info'>Role:</span> {user.role}</p>
+                            <p><span className='render-user-info'>Provider:</span> {user.provider}</p>
+                            <p><span className='render-user-info'>Stripe customer ID:</span> {user.stripeCustomerId}</p>
+                            <p><span className='render-user-info'>Created:</span> {user.createdAt.slice(0, 10)}</p>
                             <button onClick={async () => { await deleteUsers(user.id); await getAllUsers() }}>Delete User</button>
                         </div>
                     )
