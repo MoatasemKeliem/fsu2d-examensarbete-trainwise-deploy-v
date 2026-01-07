9071ec0af8f006a0779ea40ec89198ede2305505
moatasemKeliem <keliemmoatasem@gmail.com>
2026-01-07 13:15:02 +0100

    Added login input validation

--- client/src/components/LoginComponent.tsx
+++ client/src/components/LoginComponent.tsx
@@ -8,15 +8,27 @@ import { BiLogoDiscordAlt } from "react-icons/bi";
 
 const LoginComponent = () => {
     const { loginUser, loginWithDiscord, loginWithGoogle } = useAccount()
+    const [error, setError] = useState("")
     const [login, setLogin] = useState<ILogin>({
         email: "",
         password: ""
     })
 
+
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
 
-        await loginUser(login)
+        if (!login.email || !login.password) {
+            setError("Email or password is missing");
+            return
+        }
+        try {
+            setError("");
+            await loginUser(login);
+        } catch (error) {
+            setError("Invalid email or password");
+        }
+
     }
 
     const handleGoogleLogin = () => {
@@ -33,12 +45,13 @@ const LoginComponent = () => {
 
             <div className='account-div'>
                 <form onSubmit={handleSubmit} className='account-form'>
+                    {error && <p id='login-error-message'>{error}</p>}
                     <label htmlFor="">Email <br />
-                        <input type="email" name='email' value={login.email} onChange={(e) => setLogin({ ...login, email: e.target.value })} /> <br />
+                        <input className={error ? "input-error" : ""} type="email" name='email' value={login.email} onChange={(e) => setLogin({ ...login, email: e.target.value })} /> <br />
                     </label> <br />
 
                     <label htmlFor="">Password <br />
-                        <input type="password" name='password' value={login.password} onChange={(e) => setLogin({ ...login, password: e.target.value })} /> <br />
+                        <input className={error ? "input-error" : ""} type="password" name='password' value={login.password} onChange={(e) => setLogin({ ...login, password: e.target.value })} /> <br />
                     </label> <br />
                     <button type='submit'>Login</button>
                 </form>
