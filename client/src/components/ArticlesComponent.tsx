7946ade41033e9d68e7e133d0c8d441c4cd916d1
moatasemKeliem <keliemmoatasem@gmail.com>
2026-01-08 11:58:03 +0100

    added title in article page

--- client/src/components/ArticlesComponent.tsx
+++ client/src/components/ArticlesComponent.tsx
@@ -12,26 +12,39 @@ const ArticlesComponent = () => {
 
 
 
-    if (!allArticles) {
-        return <h2>There is no articles, at the moment</h2>
+    if (!allArticles.length) {
+        return (
+            <div className="no-plans">
+                <div className="admin-content">
+                    <h2> There is no articles, at the moment</h2 >
+                </div>
+            </div>
+        )
     }
 
     return (
-        <div className='render-page-div'>
-            {
-                allArticles.map((article) => {
-                    return (
-                        <div key={article.id} className='render-item'>
-                            <h3 className='render-sub-title'>{article.content?.title}</h3>
-                            <h4 className='render-category'>Caetgory: {article.category}</h4>
+        <div>
+            <div className="plans-page-title ">
+                <div className="plans-page-content">
+                    <h2>Articles</h2>
+                </div>
+            </div>
+            <div className='render-page-div'>
+                {
+                    allArticles.map((article) => {
+                        return (
+                            <div key={article.id} className='render-item'>
+                                <h3 className='render-sub-title'>{article.content?.title}</h3>
+                                <h4 className='render-category'>Caetgory: {article.category}</h4>
 
-                            <p className='render-date'>{article.createdAt.slice(0, 10)}</p>
-                            <Link to={`/articles/${article.id}`}>
-                                <button>View Article</button></Link>
-                        </div>
-                    )
-                })
-            }
+                                <p className='render-date'>{article.createdAt.slice(0, 10)}</p>
+                                <Link to={`/articles/${article.id}`}>
+                                    <button>View Article</button></Link>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
